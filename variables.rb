09c54945fe7a6ea8fc734a64fae01f876ee970bc
Chris Asercion <chris.asercion@gmail.com>
2023-03-28 15:26:46 -0600

    New edits

--- variables.rb
+++ variables.rb
@@ -1,13 +1,33 @@
-Name = "Charlie_day"
-Friend = "Day_man"
-Foe = "Night_man"
-Year = 2023
-Future = 2053
-Past = 1973
-Money = 2500.25
-Debt = 375.45
-Paid = true
-Owe = false
-Name = "King_of_day"
-Friend = "Mac"
-puts [this, is, very, confusing]
+name = "Charlie Day"
+friend = "Day Man"
+foe = "Night Man"
+year = 2023
+future = 2053
+past = 1973
+money = 2500.25
+debt = 375.45
+debt_paid = true
+pwe = false
+name = "King of Day"
+
+puts name
+puts future
+puts friend
+puts foe
+puts year
+puts money
+puts debt_paid
+
+friend = "John Tolkien"
+year = 1985
+money = 10000.01
+debt_paid = false
+ 
+puts friend
+puts year
+puts money
+puts debt_paid
+
+
+
+
